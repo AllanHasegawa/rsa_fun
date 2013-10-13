59c7b15d320b1b829372f9b3ed3cf51201616932
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-10-13 19:30:15 -0300

    full working naive implementation

--- sourcetree/main.cpp
+++ sourcetree/main.cpp
@@ -18,12 +18,41 @@ int main(void) {
 
 	cout << endl << string(25,'-') << endl;
 
+	mpz_class p{11};
+	mpz_class q{13};
+	mpz_class n{p*q};
+	mpz_class e{17};
 	mpz_class r;
-	rf::crypt(92, 17, 143, r);
+
+	// original msg
+	// crypt using plubic key values
+	rf::crypt(92, e, n, r);
 	cout << r << endl;
 
-	rf::decrypt(r.get_si(), 113, 143, r);
 
+	// now decrypt!
+	// factor the N from public key
+	mpz_class x, y;
+	rf::find_2_prime_factors_naive(n, x, y);
+	cout << x << "," << y << endl;
+
+	// fist make (p-1)*(q-1)
+	mpz_class pq{(x-1)*(y-1)};
+	cout << pq << endl;
+
+	// ed = 1 + pq(a)
+	// e is know, also "pq". Use extended euclidean to find d and a;
+	const auto ee = rf::extended_euclidean(pq,e);
+	cout << ee << endl;
+	mpz_class d{get<2>(ee)};
+	// get the next positive value of "d"
+	while (d <= 0) {
+		d += pq;
+	}
+
+	// now we have all the private key values,
+	// just decrypt :)
+	rf::decrypt(r, d, n, r);
 	cout << r << endl;
 
 	return 0;
