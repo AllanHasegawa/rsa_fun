89eef8f9e968e316f22714abf8fcb6151712c104
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-10-13 17:28:47 -0300

    guard tags and generic is_prime_naive

--- sourcetree/rfmath.hpp
+++ sourcetree/rfmath.hpp
@@ -1,3 +1,6 @@
+#ifndef RF_RFMATH_HPP
+#define RF_RFMATH_HPP
+
 #include <tuple>
 #include "gmpxx.h"
 
@@ -9,13 +12,14 @@ namespace rf {
 		if (b == 0) return make_tuple(a, 1, 0);
 		auto t = extended_euclidean(b,a%b);
 		return make_tuple(get<0>(t),
-				get<2>(t),
+					get<2>(t),
 				get<1>(t) - (a/b)*get<2>(t));
 	}
 	
-	bool is_prime_naive(const mpz_class& n) {
+	template<typename T>
+	bool is_prime_naive(const T& n) {
 		if (n <= 0) return false;
-		mpz_class i{2};
+		T i{2};
 		const auto m = sqrt(n);
 		while(i <= m) {
 			if (n%i == 0) return false;
@@ -24,3 +28,5 @@ namespace rf {
 		return true;
 	}
 } // end namespace rf
+
+#endif //RF_RFMATH_HPP
