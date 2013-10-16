290fe9da12dd6368fb2620731d71e2e56f486112
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-10-16 17:35:02 -0300

    moved tests into its proper place

--- sourcetree/main.cpp
+++ sourcetree/main.cpp
@@ -5,59 +5,12 @@
 #include "rfutils.hpp"
 #include "rsafun.hpp"
 
-int main(void) {
+int main(int argc, char* argv[]) {
 	using namespace std;
 
 	using PB = rf::PrimeBuffer<mpz_class>;
 	PB{10000};
-
 	const auto& PBv = PB::value;
 
-	cout << rf::extended_euclidean(120,67) << endl;
-
-	cout << endl << string(25,'-') << endl;
-
-	mpz_class p{11};
-	mpz_class q{13};
-	mpz_class n{p*q};
-	mpz_class e{17};
-	mpz_class r;
-
-	// original msg
-	// crypt using plubic key values
-	rf::crypt(92, e, n, r);
-	cout << r << endl;
-
-
-	// now decrypt!
-	// factor the N from public key
-	mpz_class x, y;
-	rf::find_2_prime_factors_naive(n, x, y);
-	cout << x << "," << y << endl;
-
-	mpz_class t1{71443L*102523L};
-	rf::find_2_prime_factors_blocks(t1, x, y);
-	cout << x << "," << y << endl;
-
-	// fist make (p-1)*(q-1)
-	mpz_class pq{(x-1)*(y-1)};
-	cout << pq << endl;
-
-	// ed = 1 + pq(a)
-	// e is know, also "pq". Use extended euclidean to find d and a;
-	const auto ee = rf::extended_euclidean(pq,e);
-	cout << ee << endl;
-	mpz_class d{get<2>(ee)};
-	// get the next positive value of "d"
-	while (d <= 0) {
-		d += pq;
-	}
-
-	// now we have all the private key values,
-	// just decrypt :)
-	rf::decrypt(r, d, n, r);
-	cout << r << endl;
-
-	cout << rf::is_prime_miller_rabin(213, 2) << endl;
 	return 0;
 }
