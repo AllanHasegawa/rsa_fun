ada4b85f177dfbf1a117f52070dbb86a8ab95f42
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-10-16 17:34:32 -0300

    decrypt with public key done

--- sourcetree/rsafun.hpp
+++ sourcetree/rsafun.hpp
@@ -1,6 +1,7 @@
 #ifndef RF_RSAFUN_HPP
 #define RF_RSAFUN_HPP
 
+#include <tuple>
 #include <cmath>
 #include "gmp.h"
 #include "gmpxx.h"
@@ -28,6 +29,30 @@ namespace rf {
 				C.get_mpz_t(),
 				d.get_mpz_t(),
 				n.get_mpz_t());
+	}
+
+	void decrypt_with_public_key(const mpz_class& C, const mpz_class& e,
+			const mpz_class& n, mpz_class& result)
+	{
+		mpz_class x, y;
+		rf::find_2_prime_factors_blocks(n, x, y);
+	
+		// fist make (p-1)*(q-1)
+		mpz_class pq{(x-1)*(y-1)};
+	
+		// ed = 1 + pq(a)
+		// e is know, also "pq".
+		// Use extended euclidean to find d and a;
+		const auto ee = rf::extended_euclidean(pq,e);
+		mpz_class d{std::get<2>(ee)};
+		// get the next positive value of "d"
+		while (d <= 0) {
+			d += pq;
+		}
+	
+		// now we have all the private key values,
+		// just decrypt :)
+		rf::decrypt(C, d, n, result);
 	}
 } // end namespace rf
 
