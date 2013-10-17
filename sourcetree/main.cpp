8be7c6158c9cff5c5c5ac787ee14f6dcdeb73684
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-10-17 04:11:16 -0300

    find_2_prime_factors: each thread keeps a cache of all the primes in the 210 group

--- sourcetree/main.cpp
+++ sourcetree/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
 	using PB = rf::PrimeBuffer<mpz_class>;
 	PB{100};
 
-	int precision_bits = 64;
+	int precision_bits = 32;
 	int threads = 8;
 	auto f = rf::file_to_bytes("message1");
 
@@ -22,15 +22,23 @@ int main(int argc, char* argv[]) {
 	mpz_class pq{(p-1)*(q-1)};
 
 	mpz_class n{p*q};
-	mpz_class x;
-	mpz_class y;
 
 	mpz_class e;
 	rf::random_coprime(pq, precision_bits, threads, e);
 	cout << "e: " << e << endl;
 
 
+	mpz_class x;
+	mpz_class y;
 	rf::find_2_prime_factors_blocks(n, threads, x, y);
+	cout << "n: " << n << endl;
+	cout << "xy:" << x * y << endl;
+	cout << "x: " << x << endl;
+	cout << "y: " << y << endl;
+
+	if (x*y == 0)
+		rf::find_2_prime_factors_naive(n, x, y);
+	cout << string(25, '-') << endl;
 	cout << "n: " << n << endl;
 	cout << "xy:" << x * y << endl;
 	cout << "x: " << x << endl;
