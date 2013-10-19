9b71af046400023987acdada6b065e2975e1cc7f
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-10-19 13:02:26 -0300

    breakcrypt now process in variable blocks sizes

--- sourcetree/breakcrypt.cpp
+++ sourcetree/breakcrypt.cpp
@@ -37,18 +37,33 @@ int main(int argc, char* argv[]) {
 	rf::RSAPublicKey public_key = rf::open_public_key(keyfile);
 	rf::RSAPrivateKey private_key = rf::break_crypt(public_key, threads);
 
+	auto np = public_key.n.get_mpz_t();
+	// size in bits of the public key
+	// (determines max size of the block!)
+	auto n_size = mpz_sizeinbase(np,2);
+	// 2 bits bc the function may return +/- 2 bits
+	int max_block_size = (n_size-2)/8;
+
 	vector<string> crypt_message;
 
 	ifstream ifs(cryptfile);
 	cereal::BinaryInputArchive bia(ifs);
 	bia(crypt_message);
 
+	mpz_class b;
+	auto bp = b.get_mpz_t();
 	mpz_class r;
+	auto rp = r.get_mpz_t();
 	vector<uint8_t> message;
 	for_each (crypt_message.begin(), crypt_message.end(),[&](string&  m) {
 		mpz_class C{m, 16};
 		rf::decrypt(C, private_key, r);
-		message.push_back(r.get_ui());
+		for (int i{1}; i <= max_block_size; ++i) {
+			mpz_tdiv_r_2exp(bp, rp, 8);
+			message.push_back(b.get_ui());
+
+			mpz_tdiv_q_2exp(rp, rp, 8);
+		}
 	});
 
 	rf::bytes_to_file(decryptfile, message);
